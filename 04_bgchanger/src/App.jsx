6a8c7427998c5e33d0508913f1dd8eaa81791e1a
Dhruv14066 <dhruvdhanuka315@gmail.com>
2025-07-12 12:20:18 +0530

    background changer

---
+++ 04_bgchanger/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+        <div className="fixed flex flex-wrap justify-center px-2 inset-x-0 bottom-12">
+            <div className="flex flex-wrap justify-center bg-white gap-3 shadow-lg px-2 py-3 rounded-full">
+              {["red", "green", "blue", "olive", "purple", "gray", "yellow", "black","orange", "cyan"].map((clr) => (
+                <button key={clr}
+                        onClick={() => setColor(clr)}
+                        className="outline-none px-4 py-2 rounded-3xl text-white"
+                        style={{ backgroundColor: clr }}
+                >
+                {clr}
+                </button>
+              ))}
+            </div>
+        </div>
+    </div>
+
+  );
+}
+
+export default App;
